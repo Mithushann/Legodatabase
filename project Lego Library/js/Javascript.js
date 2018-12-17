6decf41ba9b9b06af0c8d51b27385cc4172e1e45
Spectroomz <40635133+Spectroomz@users.noreply.github.com>
2018-12-17 14:20:52 +0100

    LL & JB ändrad sida med results, try to fix satser

---
+++ project Lego Library/js/Javascript.js
@@ -0,0 +1,8 @@
+//code for the navigation menu, when opened and closed
+function openNav() {
+  document.getElementById("myNav").style.width = "45%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
---
+++ project Lego Library/setinfo.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- link to the css style-->
+<link	href="css/style.css"	media="screen"	rel="stylesheet"	type="text/css"/>
+</head>
+<body>
+</body>
+</html>
